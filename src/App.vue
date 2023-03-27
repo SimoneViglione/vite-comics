a3e06b932d7e81ecfed2b0f8f1747d4ac35a6f80
Simone Viglione <zimoviglio51@gmail.com>
2023-03-27 15:19:25 +0200

    scheletro

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+
+
+import TheHeader from './components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
+
+
+export default {
+
+  components: {TheHeader, TheFooter} 
+
+}
+
+</script>
+
+<template>
+
+  <TheHeader />
+  
+  <h1>ciao</h1>
+
+  <TheFooter />
+</template>
+
+<style lang="scss">
+  @use "./styles/general.scss"
+</style>
